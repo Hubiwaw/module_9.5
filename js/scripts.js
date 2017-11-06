142df829c6fffc917fdc6742fa5fa9d71d0b0da3
Hubert Maćkowiak <mackowiak.h@wp.pl>
2017-11-06 19:21:47 +0100

    js scripts

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var buttonElements = document.getElementsByClassName("button");
+	for (var i = 0; i < buttonElements.length; i++) {
+		var textButton = buttonElements[i].innerText;
+		alert(textButton);
+}
