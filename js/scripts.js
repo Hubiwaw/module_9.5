ff558ae8083096c341d30cfb70f96a2cad8271ce
Hubiwaw <32956808+Hubiwaw@users.noreply.github.com>
2017-11-07 21:03:44 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var buttonElements = document.getElementsByClassName("button");
-var length = buttonElements.length
+var length = buttonElements.length;
 	for (var i = 0; i < length; i++) {
 		var textButton = buttonElements[i].innerText;
 		alert(textButton);
